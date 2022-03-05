74d223d5f6402ea92df30629330f405baa3021a1
UmarMtukur <umartukur55@gmail.com>
2022-03-05 20:41:55 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+	<input type="file" name="file">
+	<button type="submit" name="submit">UPLOAD</button>
+	
+</form>
+</body>
+</html>
